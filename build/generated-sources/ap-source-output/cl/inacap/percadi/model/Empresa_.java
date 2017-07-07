ba182320e0e9a9e85e3d4153fba36c18c739da40
alvarov17 <deck4rd@live.com>
2017-07-07 19:16:16 -0400

    Actualizacion

---
+++ build/generated-sources/ap-source-output/cl/inacap/percadi/model/Empresa_.java
@@ -0,0 +1,21 @@
+package cl.inacap.percadi.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Empresa.class)
+public abstract class Empresa_ {
+
+	public static volatile SingularAttribute<Empresa, String> rut;
+	public static volatile SingularAttribute<Empresa, String> nombrecontacto;
+	public static volatile SingularAttribute<Empresa, String> razonsocial;
+	public static volatile SingularAttribute<Empresa, Integer> id;
+	public static volatile SingularAttribute<Empresa, String> telefono;
+	public static volatile SingularAttribute<Empresa, String> nombre;
+	public static volatile SingularAttribute<Empresa, String> email;
+	public static volatile SingularAttribute<Empresa, String> contraseña;
+
+}
+
